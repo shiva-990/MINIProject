755f01e92299c7333a0af83d8300cf3a0236f5b8
Shiva Chandra <shivachandra151@gmail.com>
2026-04-17 16:51:50 +0530

    UC5: Implemented polymorphism using calculate_trip_cost

--- eco_rider/main.py
+++ eco_rider/main.py
@@ -1,8 +1,18 @@
 from models.electric_car import ElectricCar
 from models.electric_scooter import ElectricScooter
 
+
 car = ElectricCar("KA-01", "Nexon EV", 80, 5)
 scooter = ElectricScooter("KA-02", "Ather 450X", 60, 90)
 
-print("Car trip cost:", car.calculate_trip_cost(10))
-print("Scooter trip cost:", scooter.calculate_trip_cost(30))
+# Store different objects in one list
+vehicles = [car, scooter]
+
+
+for v in vehicles:
+    if isinstance(v, ElectricCar):
+        cost = v.calculate_trip_cost(10)   # km
+    else:
+        cost = v.calculate_trip_cost(30)   # minutes
+
+    print(f"{v.model} trip cost: {cost}")
