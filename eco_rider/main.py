73295a24a2de0da029cbd0974e23d8679a1db39b
Shiva Chandra <shivachandra151@gmail.com>
2026-04-17 16:08:02 +0530

    UC2: Encapsulation with getters and setters

--- eco_rider/main.py
+++ eco_rider/main.py
@@ -3,7 +3,11 @@ from models.vehicle import Vehicle
 
 v1 = Vehicle("KA-01-1234", "Tata Nexon EV", 80)
 
+print("Battery:", v1.get_battery())
+
+# Update battery
+v1.set_battery(90)
+print("Updated Battery:", v1.get_battery())
 
 print("Vehicle ID:", v1.vehicle_id)
 print("Model:", v1.model)
-print("Battery:", v1.battery_percentage)
