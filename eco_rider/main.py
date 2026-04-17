f335ee3df32ef2352eec13be7697468db4e379ed
Shiva Chandra <shivachandra151@gmail.com>
2026-04-17 16:55:28 +0530

    UC6: Implemented fleet management with hubs

--- eco_rider/main.py
+++ eco_rider/main.py
@@ -1,18 +1,16 @@
 from models.electric_car import ElectricCar
 from models.electric_scooter import ElectricScooter
+from services.fleet_manager import *
 
 
 car = ElectricCar("KA-01", "Nexon EV", 80, 5)
 scooter = ElectricScooter("KA-02", "Ather 450X", 60, 90)
 
-# Store different objects in one list
-vehicles = [car, scooter]
+# UC6: Fleet Management
+add_hub("Downtown")
+add_hub("Airport")
 
+add_vehicle_to_hub("Downtown", car)
+add_vehicle_to_hub("Downtown", scooter)
 
-for v in vehicles:
-    if isinstance(v, ElectricCar):
-        cost = v.calculate_trip_cost(10)   # km
-    else:
-        cost = v.calculate_trip_cost(30)   # minutes
-
-    print(f"{v.model} trip cost: {cost}")
+display_fleet()
---
+++ eco_rider/services/fleet_manager.py
@@ -0,0 +1,24 @@
+fleet_hubs = {}
+
+# Add new hub
+def add_hub(hub_name):
+    if hub_name in fleet_hubs:
+        print("Hub already exists!")
+    else:
+        fleet_hubs[hub_name] = []
+        print(f"{hub_name} hub created successfully")
+
+# Add vehicle to hub
+def add_vehicle_to_hub(hub_name, vehicle):
+    if hub_name not in fleet_hubs:
+        print("Hub does not exist!")
+    else:
+        fleet_hubs[hub_name].append(vehicle)
+        print(f"{vehicle.model} added to {hub_name}")
+
+# Display all hubs
+def display_fleet():
+    for hub, vehicles in fleet_hubs.items():
+        print(f"\nHub: {hub}")
+        for v in vehicles:
+            print(f" - {v.model} ({v.vehicle_id})")
